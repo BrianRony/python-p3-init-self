6d646a7d3ee95ea2c196297bfae3d17b95da06e3
Brian Ngetich <kipronobrian97@gmail.com>
2024-07-08 16:00:16 +0300

    Initialization

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+scoob = Dog("Scoob", "Shepherd")
+
+print(scoob.breed)
+
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name) -> None:
+        self.name = name
+
+brian = Person("Brian")
+
+print(brian.name)
